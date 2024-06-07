a8da00492941e91f686a3c519913011f95e16bac
bdshilli <bdshilli@gmail.com>
2024-06-07 12:16:24 -0400

    added mangodb access

---
+++ project-dir/server.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+//testdb is name of database, it will automatically make it
+mongoose
+  .connect(
+    "mongodb+srv://bdshilli:ZEpqGCmtrtQNz1ZW@data.hw2cdx0.mongodb.net/?retryWrites=true&w=majority&appName=Data"
+  )
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
+
+const schema = new mongoose.Schema({
+  name: String,
+});
+
+async function createMessage() {
+  const result = await message.save();
+  console.log(result);
+}
+
+//this creates a Message class in our app
+const Message = mongoose.model("Message", schema);
+const message = new Message({
+  name: "Hello World",
+});
+
+createMessage();
